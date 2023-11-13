ff3efb8cf847407f07ad6f05b01c562fc03e326c
fabdul-rah <feraidon.a.rahimzai@gmail.com>
2023-11-13 14:49:08 -0600

    Revised

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+@app.route('/eating-disorders')
+def eating_disorders():
+    return render_template('eating_disorders.html')
+
+@app.route('/suicide-prevention')
+def suicide_prevention():
+    return render_template('suicide_prevention.html')
+
+@app.route('/combination')
+def combination():
+    return render_template('combination.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
